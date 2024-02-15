97e89dc616a2852a9075ac53dabc1ac0986d984d
EcchiClone <aa01092308481@gmail.com>
2024-02-16 03:06:38 +0900

    [fix] TimeScale이 0일 경우, ESC나 P키가 작동 안하는 오류

---
+++ MeTwo/Assets/Scripts/UI/Scene/Main_CallPause.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_CallPause : MonoBehaviour
+{
+    void Update()
+    {
+        // Escape, P 키를 눌렀을 때 Pause를 열거나 팝업을 닫음
+        if (Input.GetKeyDown(KeyCode.Escape)|| Input.GetKeyDown(KeyCode.P))
+        {
+            if (TempManagers.UI.GetPopStackCount() > 0)
+            {
+                TempManagers.UI.ClosePopupUI();
+                if (TempManagers.UI.GetPopStackCount() == 0)
+                    TempManagers.SetStatePlaying();
+            }
+            else
+            {
+                TempManagers.UI.ShowPopupUI<UI_Pause>();
+                TempManagers.SetStatePause();
+            }
+        }
+    }
+}
