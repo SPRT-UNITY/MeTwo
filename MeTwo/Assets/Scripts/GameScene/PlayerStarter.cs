0c75324f71ef97027331b12367b210a8cf59f0c2
Siryu <siryu2409@gmail.com>
2024-02-15 12:15:07 +0900

    [feat] Base classes for GameScene
    Add GameSceneManager to manage game stage(pause, playing), to init game (spawn player on playerStarter and etc.)
    Add Stage class
    Add PlayerStarter to mark player spawn position
    Add ClearObject script to define stage's end

---
+++ MeTwo/Assets/Scripts/GameScene/PlayerStarter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum PlayerFlag 
+{
+    Player, Shadow
+}
+
+public class PlayerStarter : MonoBehaviour
+{
+    public PlayerFlag playerFlag;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = playerFlag == PlayerFlag.Player ? Color.green : Color.red;
+        Gizmos.DrawCube(transform.position, new Vector3(1,1,1));
+    }
+}
